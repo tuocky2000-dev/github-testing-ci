9a9fd963f93d6427e2a29b112942ba3c841d8d15
Ky Thai <tuocky2000@gmail.com>
2026-03-27 15:08:40 -0700

    add an addition

--- addition.test.js
+++ addition.test.js
@@ -8,6 +8,10 @@ test('23 + 60 = 83', () => {
     expect(addition(23, 60)).toBe(83);
 })
 
+test('3 + 6 = 9', () => {
+    expect(addition(3, 6)).toBe(9);
+})
+
 test.each([[3, 4, 7],[5, -10, -5],[0, 2, 2]])(
     'a + b = c', (a, b, expected) => {
         expect(addition(a, b)).toBe(expected);
