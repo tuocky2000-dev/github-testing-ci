c942df01f2c5557a90f555180be7f012a48164f9
Ky Thai <tuocky2000@gmail.com>
2026-03-27 15:01:19 -0700

    git repo created

---
+++ addition.test.js
@@ -0,0 +1,15 @@
+const { addition } = require('./addition');
+
+test('2 + 4 = 6', () => {
+    expect(addition(2, 4)).toBe(6);
+})
+
+test('23 + 60 = 83', () => {
+    expect(addition(23, 60)).toBe(83);
+})
+
+test.each([[3, 4, 7],[5, -10, -5],[0, 2, 2]])(
+    'a + b = c', (a, b, expected) => {
+        expect(addition(a, b)).toBe(expected);
+     }
+    )
