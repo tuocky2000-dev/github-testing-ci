13b0f106bbef89648e8fcbf24dd5572408dca1d6
Ky Thai <tuocky2000@gmail.com>
2026-03-27 15:21:55 -0700

    asdasd

--- addition.test.js
+++ addition.test.js
@@ -12,6 +12,10 @@ test('3 + 6 = 9', () => {
     expect(addition(3, 6)).toBe(9);
 })
 
+test('3 + 7 = 10', () => {
+    expect(addition(3, 6)).toBe(9);
+})
+
 test.each([[3, 4, 7],[5, -10, -5],[0, 2, 2]])(
     'a + b = c', (a, b, expected) => {
         expect(addition(a, b)).toBe(expected);
